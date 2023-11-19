977e3f37c5a38fcab1f4fd8f9983ac963246a8f5
Victor5g <vicoe.sales@gmail.com>
2023-11-19 00:06:44 -0300

    feat: add header in feed

--- src/screens/feed/style.ts
+++ src/screens/feed/style.ts
@@ -1,12 +1,36 @@
 import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import {
+  widthPercentageToDP as w,
+  heightPercentageToDP as h,
+} from "react-native-responsive-screen";
+
+import COLORS from "../../common/constants/colors";
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    paddingTop: 16,
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
+  header: {
+    paddingHorizontal: 12,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: RFValue(23),
+    fontStyle: "normal",
+    fontWeight: "600",
+    color: COLORS.primaryBlack,
+  },
+  imageProfile: {
+    width: w(13),
+    height: h(6),
+    borderRadius: 100,
+  },
 });
 
 export default style;
--- src/screens/feed/view.tsx
+++ src/screens/feed/view.tsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-
-import Illustration from "../../assets/illustration-gram.svg";
+import { Text, View, Image } from "react-native";
 
 import SafeAreaWrapper from "../../components/safeAreaWrapper";
 
 import style from "./style";
 
 const FeedView = () => {
-
   return (
     <SafeAreaWrapper>
       <View style={style.container}>
-       <Text>{"FeedView"}</Text>
+
+        <View style={style.header}>
+          <Text style={style.title}>{"Feeds"}</Text>
+          <Image
+            style={style.imageProfile}
+            source={{
+              uri: "https://reactnative.dev/img/tiny_logo.png",
+            }}
+          />
+        </View>
+
+        <Text>{"FeedView"}</Text>
       </View>
     </SafeAreaWrapper>
   );
